87da092c63112fe75c6aeaac717356930e492317
gaucon <wherezfree@hotmail.com>
2020-05-03 16:18:58 -0700

    Completed module

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 import './Tile.css'
 
+const Tile = () => {
+
 const Tile = (props) => {
   const dynamicColor = props.selected || props.matched ? { backgroundColor: props.color} : null
-
   return (
     <div className='Tile' onClick={() => { props.handleTileClicked(props.id, props.color) }} style={dynamicColor}>
     { props.selected || props.matched ? <props.svg /> : null }
     </div>
   )
 }
-
 export default Tile
